a4d1935d2e1299d75cc9261628bcef7b28154893
JC <sanjothebay@gmail.com>
2020-11-02 20:44:31 -0600

    working five day forcast

--- script.js
+++ script.js
@@ -95,77 +95,73 @@ function searchWeather(usersInput) {
             console.log(response);
 
             // Transfer content to HTML
-            $(".1stDayDay").text("Date: " + response.list[0].dt_txt);
-            $(".1stDayhumidity").text("Humidity: " + response.list[0].main.humidity);
+            $(".1stDayDay").text("Date: " + response.list[3].dt_txt);
+            $(".1stDayhumidity").text("Humidity: " + response.list[3].main.humidity);
 
-            $(".2ndDayDate").text("Date: " + response.list[1].dt_txt);
-            $(".2stDayhumidity").text("Humidity: " + response.list[1].main.humidity);
+            $(".2ndDayDate").text("Date: " + response.list[11].dt_txt);
+            $(".2ndDayhumidity").text("Humidity: " + response.list[11].main.humidity);
 
-            $(".3rdDayDate").text("Date: " + response.list[2].dt_txt);
-            $(".3stDayhumidity").text("Humidity: " + response.list[2].main.humidity);
+            $(".3rdDayDate").text("Date: " + response.list[19].dt_txt);
+            $(".3rdDayhumidity").text("Humidity: " + response.list[19].main.humidity);
 
-            $(".4thDayDate").text("Date: " + response.list[3].dt_txt);
-            $(".4stDayhumidity").text("Humidity: " + response.list[3].main.humidity);
+            $(".4thDayDate").text("Date: " + response.list[27].dt_txt);
+            $(".4thDayhumidity").text("Humidity: " + response.list[27].main.humidity);
             
-            $(".5thDayDate").text("Date: " + response.list[4].dt_txt);
-            $(".5stDayhumidity").text("Humidity: " + response.list[4].main.humidity);
+            $(".5thDayDate").text("Date: " + response.list[35].dt_txt);
+            $(".5thDayhumidity").text("Humidity: " + response.list[35].main.humidity);
 
             // Convert the temp to fahrenheit
-            let tempF = (response.list[0].main.temp - 273.15) * 1.80 + 32;
+            let dayOnetempF = (response.list[3].main.temp - 273.15) * 1.80 + 32;
+
+            let dayTwotempF = (response.list[11].main.temp - 273.15) * 1.80 + 32;
+
+            let dayThreetempF = (response.list[19].main.temp - 273.15) * 1.80 + 32;
+
+            let dayFourtempF = (response.list[27].main.temp - 273.15) * 1.80 + 32;
+
+            let dayFivetempF = (response.list[35].main.temp - 273.15) * 1.80 + 32;
 
             // add temp content to html
             //$("").text("Temperature (K) " + response.main.temp);
-            $(".1stDaytempF").text("Temperature (F) " + tempF.toFixed(2));
+            $(".1stDaytempF").text("Temperature (F) " + dayOnetempF.toFixed(2));
 
-            $(".2stDaytempF").text("Temperature (F) " + tempF.toFixed(2));
+            $(".2ndDaytempF").text("Temperature (F) " + dayTwotempF.toFixed(2));
 
-            $(".3stDaytempF").text("Temperature (F) " + tempF.toFixed(2));
+            $(".3rdDaytempF").text("Temperature (F) " + dayThreetempF.toFixed(2));
 
-            $(".4stDaytempF").text("Temperature (F) " + tempF.toFixed(2));
+            $(".4thDaytempF").text("Temperature (F) " + dayFourtempF.toFixed(2));
 
-            $(".5thDaytemp").text("Temperature (F) " + tempF.toFixed(2));
+            $(".5thDaytempF").text("Temperature (F) " + dayFivetempF.toFixed(2));
 
             // Log the data in the console as well
             console.log("Date: " + response.list[0].dt_txt);
             console.log("Humidity: " + response.list[0].main.humidity);
-            console.log("Temperature (F): " + tempF);
+           // console.log("Temperature (F): " + tempF);
 
-            let iconLink = "http://openweathermap.org/img/wn/" + response.list[0].weather[0].icon + "@2x.png";
+            let iconLink = "http://openweathermap.org/img/wn/" + response.list[3].weather[0].icon + "@2x.png";
             let imgTag = $("<img>")
             imgTag.attr("src", iconLink);
             $(".1stDayicon").html(imgTag)
 
-            // let iconLink = "http://openweathermap.org/img/wn/" + response.list[1].weather[0].icon + "@2x.png";
-            // let imgTag = $("<img>")
-            // imgTag.attr("src", iconLink);
-            // $(".1stDayicon").html(imgTag)
+            let dayoneiconLink = "http://openweathermap.org/img/wn/" + response.list[11].weather[0].icon + "@2x.png";
+            let daytwoimgTag = $("<img>")
+            daytwoimgTag.attr("src", dayoneiconLink);
+            $(".2ndDayicon").html(daytwoimgTag)
 
-            // let iconLink = "http://openweathermap.org/img/wn/" + response.list[2].weather[0].icon + "@2x.png";
-            // let imgTag = $("<img>")
-            // imgTag.attr("src", iconLink);
-            // $(".1stDayicon").html(imgTag)
+            let daytwoiconLink = "http://openweathermap.org/img/wn/" + response.list[19].weather[0].icon + "@2x.png";
+            let daythreeimgTag = $("<img>")
+            daythreeimgTag.attr("src", daytwoiconLink);
+            $(".3rdDayicon").html(daythreeimgTag)
 
-            // let iconLink = "http://openweathermap.org/img/wn/" + response.list[3].weather[0].icon + "@2x.png";
-            // let imgTag = $("<img>")
-            // imgTag.attr("src", iconLink);
-            // $(".1stDayicon").html(imgTag)
+            let daythreeiconLink = "http://openweathermap.org/img/wn/" + response.list[27].weather[0].icon + "@2x.png";
+            let dayfourimgTag = $("<img>")
+            dayfourimgTag.attr("src", daythreeiconLink);
+            $(".4thDayicon").html(dayfourimgTag)
 
-            // let iconLink = "http://openweathermap.org/img/wn/" + response.list[4].weather[0].icon + "@2x.png";
-            // let imgTag = $("<img>")
-            // imgTag.attr("src", iconLink);
-            // $(".1stDayicon").html(imgTag)
-
-
-
-
-
-
-
-
-
-
-
-
+            let dayfouriconLink = "http://openweathermap.org/img/wn/" + response.list[35].weather[0].icon + "@2x.png";
+            let dayfiveimgTag = $("<img>")
+            dayfiveimgTag.attr("src", dayfouriconLink);
+            $(".5thDayicon").html(dayfiveimgTag)
 
 
 
